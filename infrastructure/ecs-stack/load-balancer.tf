d8c36591181f682afa6a3933fef62ff4c867e170
Johnny Huynh <info@johnnyhuy.com>
2020-10-19 08:04:04 +1100

    fix: Moved a bunch of NSGs to their own file

--- infrastructure/ecs-stack/load-balancer.tf
+++ infrastructure/ecs-stack/load-balancer.tf
@@ -1,25 +1,3 @@
-resource "aws_security_group" "load_balancer" {
-  name        = "load-balancer-sg"
-  description = "Allow internet ingress from the load balancer."
-  vpc_id      = aws_vpc.this.id
-
-  ingress {
-    protocol    = "tcp"
-    from_port   = 80
-    to_port     = 80
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  tags = local.tags
-}
-
 resource "aws_lb" "this" {
   depends_on = [aws_internet_gateway.this]
 
